cf5b8cea93da8297724f75dafb263a30516b13f4
Chimeracle <29032180+chimermc@users.noreply.github.com>
2024-12-30 21:01:48 -0500

    Fix rounding for negative values

--- ArtifactsBot.Services/ArtifactsService.Simulator.cs
+++ ArtifactsBot.Services/ArtifactsService.Simulator.cs
@@ -86,7 +86,7 @@ public partial class ArtifactsService
     /// <summary>
     /// Round to the nearest int. Values ending in .5 always round up. This is the rounding logic used by the game.
     /// </summary>
-    public static int Round(double value) => (int)(value + 0.5);
+    public static int Round(double value) => (int)Math.Floor(value + 0.5);
 
     public static CharacterStats GetCharacterStats(IEnumerable<ItemSchema> characterEquipment, int characterLevel)
     {
